925b8f91c54033dd7d49b9e9a9dd355c7fd3e1ad
Dora Gil <git@doragil.me>
2019-05-29 12:29:47 -0400

    added some css and html element ids to assist page rendering. added javascript rendering function and data store

--- js/index.js
+++ js/index.js
@@ -2,6 +2,25 @@
 
 /* Template Generators */
 
+/* Data Storage */
+  const STORE = {
+    view: 'start',
+    name : 'My Project',
+    description : null,
+    authors : [],
+    sites : null,
+    license : null,
+    instructions : null,
+    screenshots : [
+        {type: 'mobile',
+        display: false,
+        url: null},
+        {type: 'desktop',
+        display: false,
+        url: null}
+    ]
+  }
+
 /* API Calls */
   function getRepoDetails (gitRepo) {
     const queryURL = `https://api.github.com/repos/${gitRepo}`
@@ -37,11 +56,12 @@
 
 /* Get Functions */
 
-/*  Gets user input from text input field. */
+  /*  Gets user input from text input field. */
   function getUserRepoInput() {
     return $('#repo-url').val();
   }
 
+  /* Parses user input to find github repo URL */
   function parseUserInput (url) {
     let githubUserRepo = url.slice(19).split('/').slice(0, 2).join('/');
     return githubUserRepo;
@@ -57,8 +77,30 @@
   }
 
 /* Rendering Functions */
+  /* Renders proper page according to STORE.view value */
+  function render() {
+    if (STORE.view === 'start') {
+      $('#landing-info').show();
+      $('#repoURLForm').show();
+      $('#readmeOptionsForm').hide();
+      $('#readmeOutputForm').hide();
+    } else if (STORE.view === 'options') {
+      //renderReadmeOptions();
+      $('#landing-info').hide();
+      $('#repoURLForm').show();
+      $('#readmeOptionsForm').show();
+      $('#readmeOutputForm').hide();
+    } else if (STORE.view === 'output') {
+      //renderOutput(); 
+      $('#landing-info').hide();
+      $('#repoURLForm').hide();
+      $('#readmeOptionsForm').hide();
+      $('#readmeOutputForm').show();
+    }
+  }
 
 /* Event Listeners */
+
   /* Watches for submit of User GitHub Repo URL */
   function watchForm() {
       $('#repoURLForm').on('submit', function(event) {
@@ -70,4 +112,13 @@
   }
 
   /* Run on Initialize */
-  $(watchForm);
+  function initializePage() {
+    //Event Listeners
+    watchForm();
+
+    //Render Function
+    render();
+  }
+
+//Run on page initialize
+  $(initializePage);
