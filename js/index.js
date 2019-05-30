d6b30f2af63ea2c1006605c8277710f08845f36f
Jonathan Skelton <jonathan.skelton@gmail.com>
2019-05-30 10:38:43 -0400

    Initial markdown template deployed

--- js/index.js
+++ js/index.js
@@ -88,7 +88,50 @@
   /* Returns Markdown to create Markdown box. */
   function generateMarkdown() {
     console.log(`markdown fired`);
-    $('#output-markdown-syntax').val(`more text`);
+    $('#output-markdown-syntax').val(`# Project Title
+
+One Paragraph of project description goes here
+
+[Live Demo]()
+
+### Installation Instructions
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+\`\`\`
+Give the example
+\`\`\`
+
+And repeat
+
+\`\`\`
+until finished
+\`\`\`
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Built With
+
+* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+    `);
   }
 
 /* Data Storage */
