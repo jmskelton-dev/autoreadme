cf69e80ce41fdf06465149d1a9f2108a064ad5a4
Jonathan Skelton <jonathan.skelton@gmail.com>
2019-05-31 20:27:12 -0400

    Add scroll to top

--- js/index.js
+++ js/index.js
@@ -444,6 +444,20 @@ ${STORE.license === '' ? `` : `\n## License\n${STORE.license}`}
     })
   }
 
+/* Scroll To Top by rdallaire */
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {
+        $('#return-to-top').fadeIn(200);
+    } else {
+        $('#return-to-top').fadeOut(200);
+    }
+  });
+  $('#return-to-top').click(function() {
+    $('body,html').animate({
+        scrollTop : 0
+    }, 500);
+  });
+
   /* Run on Initialize */
   function initializePage() {
     //Event Listeners
